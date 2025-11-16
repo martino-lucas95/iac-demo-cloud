75b8bb58a83358091825cf912e6953dd5382c663
Lucas Martino <71530057+martino-lucas95@users.noreply.github.com>
2025-11-16 11:51:18 -0300

    demo IaC

---
+++ pulumi/index.ts
@@ -0,0 +1,19 @@
+import * as k8s from "@pulumi/kubernetes";
+
+const appLabels = { app: "nginx" };
+    
+const ns = new k8s.core.v1.Namespace("iac-demo", {
+  metadata: { name: "iac-demo" },
+});
+    
+const deployment = new k8s.apps.v1.Deployment("nginx", {
+    spec: {
+        selector: { matchLabels: appLabels },
+        replicas: 1,
+        template: {
+            metadata: { labels: appLabels },
+            spec: { containers: [{ name: "nginx", image: "nginx" }] }
+        }
+    }
+});
+export const name = deployment.metadata.name;
---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+resource "kubernetes_namespace" "iac_demo" {
+  metadata {
+    name = "iac-demo"
+  }
+}
+
