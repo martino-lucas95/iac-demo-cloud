5aed90a78e4fc927d62635fe8f538bafd09c9269
Lucas Martino <71530057+martino-lucas95@users.noreply.github.com>
2025-11-16 12:40:59 -0300

    update terraform & pulumi

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,43 @@
-terraform {
-  required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
-    }
-  }
-}
-
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-}
-
 resource "kubernetes_namespace" "iac_demo" {
   metadata {
     name = "iac-demo"
   }
 }
 
+resource "kubernetes_deployment" "hello_app" {
+  metadata {
+    name      = "hello-app"
+    namespace = kubernetes_namespace.iac_demo.metadata[0].name
+    labels = {
+      app = "hello"
+    }
+  }
+
+  spec {
+    replicas = 3
+
+    selector {
+      match_labels = {
+        app = "hello"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "hello"
+        }
+      }
+
+      spec {
+        container {
+          image = "nginx"
+          name  = "hello"
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
