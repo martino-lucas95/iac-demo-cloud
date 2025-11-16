65025ca9926384490ba91459cb89ede90533e24f
Lucas Martino <71530057+martino-lucas95@users.noreply.github.com>
2025-11-16 12:27:35 -0300

    update terraform & pulumi

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -6,6 +6,17 @@ const ns = new k8s.core.v1.Namespace("iac-demo", {
   metadata: { name: "iac-demo" },
 });
     
+const config = new k8s.core.v1.ConfigMap("app-config", {
+  metadata: {
+    name: "app-config",
+    namespace: "iac-demo",
+  },
+  data: {
+    message: "Hello from Pulumi!",
+    environment: "dev",
+  },
+});
+    
 const deployment = new k8s.apps.v1.Deployment("nginx", {
     spec: {
         selector: { matchLabels: appLabels },
