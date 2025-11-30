2abed986b9c09eb84564b09227f6d866ff7a2dc2
Lucas Martino <71530057+martino-lucas95@users.noreply.github.com>
2025-11-30 13:09:16 -0300

    update

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,30 +1,44 @@
 import * as k8s from "@pulumi/kubernetes";
 
+// 1. Crear el Namespace
+const ns = new k8s.core.v1.Namespace("iac-pulumi", {
+    metadata: { name: "iac-pulumi" }
+});
+
+// 2. Crear el Deployment
 const appLabels = { app: "nginx" };
-    
-const ns = new k8s.core.v1.Namespace("iac-demo", {
-  metadata: { name: "iac-demo" },
-});
-    
-const config = new k8s.core.v1.ConfigMap("app-config", {
-  metadata: {
-    name: "app-config",
-    namespace: "iac-demo",
-  },
-  data: {
-    message: "Hello from Pulumi!",
-    environment: "dev",
-  },
-});
-    
 const deployment = new k8s.apps.v1.Deployment("nginx", {
+    metadata: { 
+        namespace: ns.metadata.name
+     },
     spec: {
         selector: { matchLabels: appLabels },
         replicas: 1,
         template: {
             metadata: { labels: appLabels },
-            spec: { containers: [{ name: "nginx", image: "nginx" }] }
+            spec: { 
+                containers: [{ 
+                    name: "nginx",
+                    image: "nginx",
+                    imagePullPolicy: "IfNotPresent",
+                    ports: [{ containerPort: 80 }] 
+                }] 
+            }
         }
     }
 });
+
+// 3. Crear el Service
+const service = new k8s.core.v1.Service("nginx", {
+    metadata: { 
+        name: "nginx", 
+        namespace: ns.metadata.name },
+    spec: {
+        ports: [{ port: 80, targetPort: 80 }],
+        selector: appLabels,
+        type: "LoadBalancer"
+    }
+});
+
+// 4. Exportar el nombre del Deployment
 export const name = deployment.metadata.name;
--- terraform/main.tf
+++ terraform/main.tf
@@ -11,8 +11,61 @@ provider "kubernetes" {
   config_path = "~/.kube/config"
 }
 
-resource "kubernetes_namespace" "iac_demo" {
+resource "kubernetes_namespace" "iac_terraform" {
   metadata {
-    name = "iac-demo"
+    name = "iac-terraform"
+  }
+}
+
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name      = "nginx-tf"
+    namespace = kubernetes_namespace.iac_terraform.metadata.0.name
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "nginx-tf"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "nginx-tf"
+        }
+      }
+
+      spec {
+        container {
+          image = "nginx:latest"
+          name  = "nginx"
+          
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name      = "nginx-tf"
+    namespace = kubernetes_namespace.iac_terraform.metadata.0.name
+  }
+  spec {
+    selector = {
+      app = kubernetes_deployment.nginx.spec.0.selector.0.match_labels.app
+    }
+    port {
+      port        = 80
+      target_port = 80
+    }
+    type = "LoadBalancer"
   }
 }
